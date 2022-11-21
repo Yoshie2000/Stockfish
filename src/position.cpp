8890aa48452229b9158e024569a58268458763b2
Yoshie2000 <patrick.leonhardt@gmx.net>
2022-11-21 15:59:16 +0100

    Try without repetitions, with bigger increase/decrease

--- src/position.cpp
+++ src/position.cpp
@@ -775,7 +775,6 @@ void Position::do_move(Move m, StateInfo& newSt, bool givesCheck) {
 
       // Reset rule 50 counter
       st->rule50 = 0;
-      st->repetitionSinceLastReset = false;
   }
 
   // Update hash key
@@ -855,7 +854,6 @@ void Position::do_move(Move m, StateInfo& newSt, bool givesCheck) {
 
       // Reset rule 50 draw counter
       st->rule50 = 0;
-      st->repetitionSinceLastReset = false;
   }
 
   // Set capture piece
@@ -890,8 +888,6 @@ void Position::do_move(Move m, StateInfo& newSt, bool givesCheck) {
           }
       }
   }
-  if (st->repetition)
-      st->repetitionSinceLastReset = true;
 
   assert(pos_is_ok());
 }
@@ -1027,7 +1023,6 @@ void Position::do_null_move(StateInfo& newSt) {
   set_check_info(st);
 
   st->repetition = 0;
-  st->repetitionSinceLastReset = false;
 
   assert(pos_is_ok());
 }
@@ -1191,7 +1186,17 @@ bool Position::is_draw(int ply) const {
 // of positions since the last capture or pawn move.
 
 bool Position::has_repeated() const {
-    return st->repetitionSinceLastReset;
+
+    StateInfo* stc = st;
+    int end = std::min(st->rule50, st->pliesFromNull);
+    while (end-- >= 4)
+    {
+        if (stc->repetition)
+            return true;
+
+        stc = stc->previous;
+    }
+    return false;
 }
 
 
--- src/position.h
+++ src/position.h
@@ -47,7 +47,6 @@ struct StateInfo {
   int    rule50;
   int    pliesFromNull;
   Square epSquare;
-  bool   repetitionSinceLastReset;
 
   // Not copied when making a move (will be recomputed anyhow)
   Key        key;
