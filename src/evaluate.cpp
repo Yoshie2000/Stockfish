9f5b3ddc67e9b0cef4d432691cb6aa59cf349bb0
Yoshie2000 <patrick.leonhardt@gmx.net>
2022-11-21 09:13:20 +0100

    Remove damping for 50 move rule

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1084,9 +1084,6 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
       v = (nnue * scale + optimism * (scale - 754)) / 1024;
   }
 
-  // Damp down the evaluation when shuffling
-  v = v * (195 - pos.rule50_average()) / 211;
-
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
 
