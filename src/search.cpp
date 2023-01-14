64e95a542e266d431438a0b42482d61e8af72667
Yoshie2000 <patrick.leonhardt@gmx.net>
2023-01-14 13:52:21 +0100

    lmr extension take 3. Bench: 3949949

--- src/search.cpp
+++ src/search.cpp
@@ -1123,10 +1123,7 @@ moves_loop: // When in check, search starts here
       // Step 16. Make the move
       pos.do_move(move, st, givesCheck);
 
-      Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
-
-      if (extension < 0)
-          r -= extension;
+      Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta) - extension;
 
       // Decrease reduction if position is or has been on the PV
       // and node is not likely to fail low. (~3 Elo)
