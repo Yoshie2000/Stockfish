a4e0bcd71110c1f4fbb71e16382e72d3e7db9816
Yoshie2000 <patrick.leonhardt@gmx.net>
2023-02-10 20:30:04 +0100

    Try some things with complexity, take 2. Bench: 4428908

--- src/search.cpp
+++ src/search.cpp
@@ -787,7 +787,7 @@ namespace {
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
         &&  depth < 8
-        &&  eval - futility_margin(depth, improving) - (ss-1)->statScore / 304 + complexity / 36 >= beta
+        &&  eval - futility_margin(depth, improving) - (ss-1)->statScore / 304 - complexity / 36 >= beta
         &&  eval >= beta
         &&  eval < 28580) // larger than VALUE_KNOWN_WIN, but smaller than TB wins
         return eval;
