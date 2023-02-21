ab2bdad04ce3eaa10ccc83fca540fadacbea79d2
Yoshie2000 <patrick.leonhardt@gmx.net>
2023-02-21 17:31:27 +0100

    Even more bonus if move already was a killer. Bench: 4650379

--- src/search.cpp
+++ src/search.cpp
@@ -1772,7 +1772,7 @@ moves_loop: // When in check, search starts here
     Color us = pos.side_to_move();
     Thread* thisThread = pos.this_thread();
     thisThread->mainHistory[us][from_to(move)] << bonus;
-    update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), bonus + wasAlreadyKiller * 350);
+    update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), bonus + wasAlreadyKiller * 700);
 
     // Update countermove history
     if (is_ok((ss-1)->currentMove))
