d28eb8961cc5b2cb689aec5dd88de1edd72d649b
Yoshie2000 <patrick.leonhardt@gmx.net>
2023-03-07 12:04:27 +0100

    make double extension offset a function of depth, take 2. Bench: 4908352

--- src/search.cpp
+++ src/search.cpp
@@ -1086,7 +1086,7 @@ moves_loop: // When in check, search starts here
 
                   // Avoid search explosion by limiting the number of double extensions
                   if (  !PvNode
-                      && value < singularBeta - 5 * depth
+                      && value < singularBeta - 4 * depth
                       && ss->doubleExtensions <= 10)
                   {
                       extension = 2;
