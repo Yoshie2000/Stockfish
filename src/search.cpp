24ce6fd2c9dcd302cee5653a7d940150892e836f
Yoshie2000 <patrick.leonhardt@gmx.net>
2023-03-10 21:40:44 +0100

    likely fail high, take 2. Bench: 4410458

--- src/search.cpp
+++ src/search.cpp
@@ -1151,11 +1151,14 @@ moves_loop: // When in check, search starts here
       pos.do_move(move, st, givesCheck);
 
       // Decrease reduction if position is or has been on the PV
-      // and node is not likely to fail (~3 Elo)
+      // and node is not likely to fail low. (~3 Elo)
       if (   ss->ttPv
-          && !likelyFailLow
-          && !likelyFailHigh)
+          && !likelyFailLow)
           r -= 2;
+      
+      if (   ss->ttPv
+          && likelyFailHigh)
+          r++;
 
       // Decrease reduction if opponent's move count is high (~1 Elo)
       if ((ss-1)->moveCount > 7)
