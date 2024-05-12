d695ef2719b62c0015318fd8ab927f9c63972d1a
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-05-12 12:12:25 +0200

    Reduce probcutbeta if opponent worsening, take 2. Bench: 2435924

--- src/search.cpp
+++ src/search.cpp
@@ -834,7 +834,7 @@ Value Search::Worker::search(
     // Step 11. ProbCut (~10 Elo)
     // If we have a good enough capture (or queen promotion) and a reduced search returns a value
     // much above beta, we can (almost) safely prune the previous move.
-    probCutBeta = beta + 159 - 66 * improving;
+    probCutBeta = beta + 159 - 66 * improving - 33 * (opponentWorsening && !improving);
     if (
       !PvNode && depth > 3
       && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
