3d73c0f584d31f7accc6c4184453757356142aba
Yoshie2000 <patrick.leonhardt@gmx.net>
2024-06-06 22:09:32 +0200

    More threshold. Bench: 1168543

--- src/search.cpp
+++ src/search.cpp
@@ -1151,7 +1151,7 @@ moves_loop:  // When in check, search starts here
         if (ttCapture)
             r++;
         
-        if (unadjustedStaticEval < alpha && ss->staticEval > beta)
+        if (unadjustedStaticEval + 15 < alpha && ss->staticEval > beta)
             r++;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
